d870de41602a37f8dba6b0447c94d820a76044e8
EunHyeokJung <silverhyeok.dev@gmail.com>
2024-09-13 13:58:52 +0900

    분기점: 기구관리 추가 및 병실환경과 중복되지 않도록 로직 구성
    - 레이아웃 관련 오류 있음

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -7,15 +7,32 @@ import Button from "../Button";
 import Setting from "./Setting";
 import styles from "./HeaderContainer.module.css";
 
-const HeaderContainer = ({ setRoomAir,setSelects, setMenuTab, setTab, menuTab }) => {
-
+const HeaderContainer = ({
+  setRoomAir,
+  setEquipment,
+  setSelects,
+  setMenuTab,
+  setTab,
+  menuTab,
+  roomAir,
+  equipment,
+}) => {
   return (
     <div className={styles.main}>
       <Header>
         <Eums />
-        <ViewName text="이름보기" onClick={()=> setSelects(prev => !prev)} />
+        <ViewName text="이름보기" onClick={() => setSelects((prev) => !prev)} />
         <Fieldset />
-        <ViewName text="병실환경" onClick={()=> setRoomAir(prev => !prev)}/>
+        <ViewName
+          text="병실환경"
+          onClick={setRoomAir}
+          checked={roomAir} // Bind roomAir state
+        />
+        <ViewName
+          text="기구관리"
+          onClick={setEquipment}
+          checked={equipment} // Bind equipment state
+        />
         <Menus menuTab={menuTab} setMenuTab={setMenuTab} setTab={setTab} />
         <Setting />
       </Header>
--- src/pages/RoomMain.jsx
+++ src/pages/RoomMain.jsx
@@ -1,26 +1,56 @@
-import HeaderContainer from "../components/Header/HeaderContainer"
-import RoomContainer from "../components/Room/RoomContainer"
-import Bottom from "../components/Bottom/Bottom"
-import styles from "./RoomMain.module.css"
-import { useState } from "react"
+import HeaderContainer from "../components/Header/HeaderContainer";
+import RoomContainer from "../components/Room/RoomContainer";
+import Bottom from "../components/Bottom/Bottom";
+import styles from "./RoomMain.module.css";
+import { useState } from "react";
 
-const RoomMain = () =>{
-  const [menuTab, setMenuTab] = useState(null)
-  const [isTab, setTab] = useState(false)
-  const [selects,setSelects] = useState(false)
-  const [roomAir, setRoomAir] = useState(false)
-  
+const RoomMain = () => {
+  const [menuTab, setMenuTab] = useState(null);
+  const [isTab, setTab] = useState(false);
+  const [selects, setSelects] = useState(false);
+  const [roomAir, setRoomAir] = useState(false);
+  const [equipment, setEquipment] = useState(false);
 
-  
-  
+  const setRoomAirHandler = () => {
+    if (!roomAir) {
+      setRoomAir(true);
+      setEquipment(false);
+    } else {
+      setRoomAir(false);
+    }
+  };
+
+  const setEquipmentHandler = () => {
+    if (!equipment) {
+      setEquipment(true);
+      setRoomAir(false);
+    } else {
+      setEquipment(false);
+    }
+  };
 
   return (
     <div className={styles.roomContainer}>
-      <HeaderContainer setRoomAir={setRoomAir} setSelects={setSelects} menuTab={menuTab} setTab={setTab} setMenuTab={setMenuTab}/>
-      <RoomContainer roomAir={roomAir} selects={selects} menuTab={menuTab} isTab={isTab} setTab={setTab}/>
-      <Bottom/>
+      <HeaderContainer
+        setRoomAir={setRoomAirHandler}
+        setSelects={setSelects}
+        menuTab={menuTab}
+        setTab={setTab}
+        setMenuTab={setMenuTab}
+        setEquipment={setEquipmentHandler}
+        roomAir={roomAir}
+        equipment={equipment}
+      />
+      <RoomContainer
+        roomAir={roomAir}
+        selects={selects}
+        menuTab={menuTab}
+        isTab={isTab}
+        setTab={setTab}
+      />
+      <Bottom />
     </div>
-  )
-}
+  );
+};
 
-export default RoomMain
+export default RoomMain;
