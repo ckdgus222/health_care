87a0bc1af05f816da408124e6c50c005c5a5fd29
EunHyeokJung <silverhyeok.dev@gmail.com>
2024-09-19 15:41:38 +0900

    Fix: emergency box가 두번 랜더링 되는 버그 수정
    - renderAlerts가 map 함수 안에 있는 것이 문제

--- src/components/Eum/EndoRoom.jsx
+++ src/components/Eum/EndoRoom.jsx
@@ -1,6 +1,6 @@
 import styles from "./EndoRoom.module.css";
 import Alert from "./Alert";
-import { useState,useLayoutEffect,useEffect,useCallback,useMemo } from "react";
+import { useState, useLayoutEffect, useEffect, useCallback, useMemo } from "react";
 
 
 // inner-grid 반복 더미
@@ -22,7 +22,6 @@ const EndoRoom = ({ tempData, setTempData }) => {
     // [0,1,null,null,,,,] 모든 요소를 순회후 데이터 있는 값만 새로운 배열로 만든후
     // filter를 사용해 데이터가 있는 값만 필터링
     const emergency = tempData.map((item, index) => (item.condition ? index : null)).filter((index) => index !== null);
-
     setSelectedPatient(emergency);
   }, [tempData]);
 
@@ -31,9 +30,9 @@ const EndoRoom = ({ tempData, setTempData }) => {
       const recoveryTime = item.recovery;
       const firstStageEnd = recoveryTime - intervalTime;
       const intervalDuration = (recoveryTime / totalDuration) * 6000; // * 10 * 60 * 1000 10분기준
-  
+
       let currentStep = 0;
-  
+
       const firstInterval = setInterval(() => {
         if (currentStep * intervalTime < firstStageEnd) {
           setCurrentValue((prevValues) => {
@@ -51,7 +50,7 @@ const EndoRoom = ({ tempData, setTempData }) => {
               return newValues;
             });
           }, 1000); // 깜빡이는 속도 (1초)
-  
+
           setTimeout(() => {
             clearInterval(blinkInterval);
             setCurrentValue((prevValues) => {
@@ -62,11 +61,10 @@ const EndoRoom = ({ tempData, setTempData }) => {
           }, (intervalTime / 10) * 60 * 1000); // 깜빡임 종료 시간 (예 : 10분)
         }
       }, intervalDuration);
-  
+
       return firstInterval;
     });
 
-  
     return () => intervals.forEach((interval) => clearInterval(interval));
   }, [tempData, intervalTime, totalDuration]);
 
@@ -89,95 +87,98 @@ const EndoRoom = ({ tempData, setTempData }) => {
   }, [tempData]);
 
   const handlePatientClick = useCallback(
-    (i) => {
-      setSelectedPatient((prev) => {
-        if (prev[prev.length - 1] === i) return prev;
-        return [...prev.filter((item) => item !== i), i];
-      });
-    },
-    [setSelectedPatient]
+      (i) => {
+        setSelectedPatient((prev) => {
+          if (prev[prev.length - 1] === i) return prev;
+          return [...prev.filter((item) => item !== i), i];
+        });
+      },
+      [setSelectedPatient]
   );
 
   const emergencyClick = useCallback(
-    (index) => {
-      setTempData((prevTempData) => {
-        const updatedData = prevTempData.map((patient, i) => {
-          if (i === index && patient.condition !== "") {
-            return { ...patient, condition: "" };
-          }
-          return patient;
+      (index) => {
+        setTempData((prevTempData) => {
+          const updatedData = prevTempData.map((patient, i) => {
+            if (i === index && patient.condition !== "") {
+              return { ...patient, condition: "" };
+            }
+            return patient;
+          });
+          return updatedData; // 변경된 데이터가 있는 경우에만 업데이트
         });
-        return updatedData; // 변경된 데이터가 있는 경우에만 업데이트
-      });
-      setSelectedPatient((prev) => prev.filter((item) => item !== index));
-    },
-    [setTempData, setSelectedPatient]
+        setSelectedPatient((prev) => prev.filter((item) => item !== index));
+      },
+      [setTempData, setSelectedPatient]
   );
 
-const renderAlerts = useMemo(() => {
-  return selectedPatient.map((patientIndex) => {
-    const selectedItem = tempData[patientIndex];
-    if (!selectedItem || !selectedItem.condition) return null;
+  const renderAlerts = useMemo(() => {
+    return selectedPatient.map((patientIndex) => {
+      const selectedItem = tempData[patientIndex];
+      if (!selectedItem || !selectedItem.condition) return null;
 
-    return (
-      <Alert
-        key={patientIndex}
-        item={selectedItem}
-        selectedPatient={selectedPatient}
-        currentValue={currentValue[patientIndex]}
-        totalDuration={totalDuration}
-        recoveryOut={recoveryOut[patientIndex]}
-        onConfirm={() => emergencyClick(patientIndex)}
-      />
-    );
-  });
-}, [selectedPatient, tempData, currentValue, totalDuration, recoveryOut, emergencyClick]);
+      return (
+          <Alert
+              key={patientIndex}
+              item={selectedItem}
+              selectedPatient={selectedPatient}
+              currentValue={currentValue[patientIndex]}
+              totalDuration={totalDuration}
+              recoveryOut={recoveryOut[patientIndex]}
+              onConfirm={() => emergencyClick(patientIndex)}
+          />
+      );
+    });
+  }, [selectedPatient, tempData, currentValue, totalDuration, recoveryOut, emergencyClick]);
 
 
 
   // {`${Math.floor(Math.random() * 150) + 50}/${Math.floor(Math.random() * 100)}`} (그리드 텍스트 심박수 )
 
   return (
-    <div className={styles.endoContainer}>
-      <div className={styles.outerGrid}>
-        {tempData.map((item, i) => (
-          <div
-            className={`${styles.innerGrid} ${item.condition ? styles.emergencyPatient : ""}  ${
-              item.condition ? (selectedPatient[selectedPatient.length - 1] === i ? styles.emergencyFocuse : "") : null
-            }`}
-            key={item.patNumber}
-            onClick={() => handlePatientClick(i)}
-          >
-            <div className={`${styles.innerGridImg}`} style={{ backgroundImage: `url(/images/Image/${item.positionStatus}.gif)` }}>
-              <img src={`/images/Back-img/bed/${item.barStatus}.png`} alt="" />
-            </div>
-            <div className={styles.innerGridBar}>
+      <div className={styles.endoContainer}>
+        <div className={styles.outerGrid}>
+          {tempData.map((item, i) => (
               <div
-                className={styles.recoveryTime}
-                style={{
-                  width: `${(currentValue[i] / totalDuration) * 100}%`,
-                  backgroundColor: "green",
-                  transition: "width 0.5s ease-in-out",
-                }}
-              ></div>
-            </div>
-            <div style={{ display: "flex", gap: "5px" }}>
-              <div className={styles.innerGridText} style={{ color: recoveryOut[i] >= item.recovery ? "red" : "white" }}>{`${recoveryOut[i]}M`}</div>
-              <div className={styles.innerGridText}>{`G${i + 5}`}</div>
-            </div>
-            <div className={styles.innerGridText} style={{ gridArea: "5/3/6/5" }}>
-              {item.ecgRate}
-            </div>
-            <div className={styles.innerGridText} style={{ color: "dimgray" }}>
-              {item.breathRate}
-            </div>
-            <div className={styles.innerGridText}>93/82</div>
-            <div className={styles.innerGridText}></div>
-            {renderAlerts}
-          </div>
-        ))}
+                  className={`${styles.innerGrid} ${item.condition ? styles.emergencyPatient : ""} ${
+                      item.condition ? (selectedPatient[selectedPatient.length - 1] === i ? styles.emergencyFocuse : "") : ""
+                  }`}
+                  key={item.patNumber}
+                  onClick={() => handlePatientClick(i)}
+              >
+                <div
+                    className={`${styles.innerGridImg}`}
+                    style={{ backgroundImage: `url(/images/Image/${item.positionStatus}.gif)` }}
+                >
+                  <img src={`/images/Back-img/bed/${item.barStatus}.png`} alt="" />
+                </div>
+                <div className={styles.innerGridBar}>
+                  <div
+                      className={styles.recoveryTime}
+                      style={{
+                        width: `${(currentValue[i] / totalDuration) * 100}%`,
+                        backgroundColor: "green",
+                        transition: "width 0.5s ease-in-out",
+                      }}
+                  ></div>
+                </div>
+                <div style={{ display: "flex", gap: "5px" }}>
+                  <div className={styles.innerGridText} style={{ color: recoveryOut[i] >= item.recovery ? "red" : "white" }}>{`${recoveryOut[i]}M`}</div>
+                  <div className={styles.innerGridText}>{`G${i + 5}`}</div>
+                </div>
+                <div className={styles.innerGridText} style={{ gridArea: "5/3/6/5" }}>
+                  {item.ecgRate}
+                </div>
+                <div className={styles.innerGridText} style={{ color: "dimgray" }}>
+                  {item.breathRate}
+                </div>
+                <div className={styles.innerGridText}>93/82</div>
+                <div className={styles.innerGridText}></div>
+              </div>
+          ))}
+        </div>
+        {renderAlerts}
       </div>
-    </div>
   );
 };
 
