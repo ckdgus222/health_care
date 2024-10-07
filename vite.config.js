1e0876978c46486ee8c4b3f21c649b0efac70781
CCH <ckdgus1308@naver.com>
2024-10-07 16:14:47 +0900

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
+
+// 아이콘 이미지 사이즈 변경 옵션
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Endo',
+        short_name: 'care',
+        start_url: "/",
+        display:"fullscreen",
+        description: 'health_care',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/main-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/main-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/main-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
