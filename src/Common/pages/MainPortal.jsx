624f63ccbf770c9fb9e661affd4ad542ec39aeee
EunHyeokJung <silverhyeok.dev@gmail.com>
2024-10-11 13:32:55 +0900

    Feat: Navigate onclick, button animate on hover

--- src/Common/pages/MainPortal.jsx
+++ src/Common/pages/MainPortal.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './MainPortal.module.css';
 
 const MainPortal = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <div className={styles.portalContainer}>
             <div className={styles.logoContainer}>
@@ -9,19 +16,19 @@ const MainPortal = () => {
             </div>
             <div className={styles.serviceContainer}>
                 <div className={styles.servicesGrid}>
-                    <div className={styles.serviceItem}>
+                    <div className={styles.serviceItem} onClick={() => handleNavigation('/roommain')}>
                         <img src="/images/App-img/care.png" alt="EUM:care" className={styles.icon}/>
                         <p className={styles.serviceText}>EUM:care</p>
                     </div>
-                    <div className={styles.serviceItem}>
+                    <div className={styles.serviceItem} onClick={() => handleNavigation('/roommain')}>
                         <img src="/images/App-img/care.png" alt="EUM:care" className={styles.icon}/>
                         <p className={styles.serviceText}>EUM:care</p>
                     </div>
-                    <div className={styles.serviceItem}>
+                    <div className={styles.serviceItem} onClick={() => handleNavigation('/eum')}>
                         <img src="/images/App-img/endo.png" alt="EUM:endo" className={styles.icon}/>
                         <p className={styles.serviceText}>EUM:endo</p>
                     </div>
-                    <div className={styles.serviceItem}>
+                    <div className={styles.serviceItem} onClick={() => handleNavigation('/eum')}>
                         <img src="/images/App-img/endo.png" alt="EUM:endo" className={styles.icon}/>
                         <p className={styles.serviceText}>EUM:endo</p>
                     </div>
